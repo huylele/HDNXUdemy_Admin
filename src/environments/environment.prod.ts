ea9524cea13b01675cdf9c8abb7c7c16892ef215
anglesen1120 <anglesen1120@outlook.com>
2024-03-14 08:52:53 +0700

    update config for project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,16 @@
 export const environment = {
   production: true,
+  baseUrl: 'https://hdnx-udemy-api.devproinsights.com/api/v1/',
+  urlUploadFile : 'https://hdnx-udemy-convert-video.devproinsights.com/api/v1/',
   defaultauth: 'fakebackend',
   firebaseConfig: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
+    apiKey: "AIzaSyCqS9cSPrDCNSQ-Ku2kZf5DBWjPPv7hvcA",
+    authDomain: "test-demo-774f8.firebaseapp.com",
+    databaseURL: "https://test-demo-774f8-default-rtdb.firebaseio.com",
+    projectId: "test-demo-774f8",
+    storageBucket: "test-demo-774f8.appspot.com",
+    messagingSenderId: "916438010670",
+    appId: "1:916438010670:web:c70cf404da6c0fe7b048bf",
+    measurementId: "G-1N6FB2GG55",
   }
 };
